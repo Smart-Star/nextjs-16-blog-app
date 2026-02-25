53dae870454d315972ca70a80085f37ab77c8ecd
Smart-Star <folux31@gmail.com>
2026-02-25 08:33:30 +0100

    added image size limit to next config

--- next.config.ts
+++ next.config.ts
@@ -14,6 +14,11 @@ const nextConfig: NextConfig = {
         protocol: "https",
         port: "",
       },
+      {
+        hostname: "wry-anaconda-338.eu-west-1.convex.cloud",
+        protocol: "https",
+        port: "",
+      },
     ],
   },
 };
